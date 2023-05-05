9d9d22cd852612318316534b30603e2329a9abe9
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-05 16:31:44 +0530

    header comp created, style added to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,3 +1,5 @@
+import './nav.css';
+
 import React, { useContext } from 'react';
 
 import { ApiDataContext } from '../../dataProvider/DataProvider';
@@ -6,18 +8,18 @@ const Nav = () => {
   const { setIsLight } = useContext(ApiDataContext);
 
   return (
-    <div>
-      <div>
-        <p>Where in the world?</p>
+    <div className='nav-cnt'>
+      <div className='nav-para-cnt'>
+        <p className='para'>Where in the world?</p>
       </div>
-      <div>
-        <button onClick={setIsLight(false)}>
+      <div className='nav-btn-cnt'>
+        <button className='nav-btn' onClick={setIsLight(false)}>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             width='16'
             height='16'
             fill='currentColor'
-            className='bi bi-moon-fill'
+            className=' bi-moon-fill'
             viewBox='0 0 16 16'
           >
             <path d='M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z' />
