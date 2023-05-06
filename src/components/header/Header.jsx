1beab694cc656566106749be66c019fff275ae37
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-06 13:37:21 +0530

    header comp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
-
+import { BsSearch } from 'react-icons/bs';
 import './header.css';
 
 const Header = () => {
-  const [isSelect, setIsSelect] = useState(true);
+  const [isSelect, setIsSelect] = useState(false);
 
   const handleClick = () => {
-    setIsSelect(true);
+    if (!isSelect) {
+      setIsSelect(true);
+    } else if (isSelect) {
+      setIsSelect(false);
+    }
   };
 
   return (
     <div>
       <form>
         <button>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='16'
-            height='16'
-            fill='currentColor'
-            className=' bi-search'
-            viewBox='0 0 16 16'
-          >
-            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-          </svg>
+          <BsSearch />
         </button>
         <input type='search' className='search-box' />
       </form>
@@ -30,7 +25,7 @@ const Header = () => {
         <button onClick={handleClick} className=''>
           Filter by Region v
         </button>
-        <div className={`select-btn-cnt ${isSelect ? 'active' : 'inactive'}`}>
+        <div className={`select-btn-cnt ${!isSelect ? 'active' : 'inactive'}`}>
           <button className='select-btn'>Africa</button>
           <button className='select-btn'>America</button>
           <button className='select-btn'>Asia</button>
