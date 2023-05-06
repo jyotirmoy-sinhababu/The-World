f622170e66934ded7e38691e3e598697211bc605
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-07 00:12:47 +0530

    dark mode added to home page and header-cnt

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+
 import { BsSearch } from 'react-icons/bs';
 import './header.css';
 
+import { useContext } from 'react';
+import { ApiDataContext } from '../../dataProvider/DataProvider';
+
 const Header = () => {
+  const { isLight } = useContext(ApiDataContext);
+
   const [isSelect, setIsSelect] = useState(false);
 
   const handleClick = () => {
@@ -14,27 +20,77 @@ const Header = () => {
   };
 
   return (
-    <div className='header-cnt'>
-      <form className='header-frm'>
-        <button className='header-search-btn'>
+    <div className={`header-cnt ${!isLight ? 'header-dark' : 'header-light'}`}>
+      <form
+        className={`header-frm ${
+          !isLight ? 'header-frm-dark' : 'header-frm-light'
+        }`}
+      >
+        <button
+          className={`header-search-btn ${
+            !isLight ? 'search-btn-dark' : 'search-btn-light'
+          }`}
+        >
           <BsSearch />
         </button>
         <input
           type='search'
-          className='search-box'
+          className={!isLight ? 'search-box-dark' : 'search-box'}
           placeholder='search for country...'
         />
       </form>
-      <div className='header-drpDownnBtn-cnt'>
-        <button onClick={handleClick} className='header-drp-btn'>
+
+      <div
+        className={`header-drpDownnBtn-cnt ${
+          !isLight
+            ? 'header-drpDownnBtn-cnt-dark'
+            : 'header - drpDownnBtn - cnt'
+        }`}
+      >
+        <button
+          onClick={handleClick}
+          className={`header-drp-btn ${
+            !isLight ? 'header-drp-btn-dark' : 'header-drp-btn'
+          }`}
+        >
           Filter by Region v
         </button>
         <div className={`select-btn-cnt ${!isSelect ? 'active' : 'inactive'}`}>
-          <button className='select-btn'>Africa</button>
-          <button className='select-btn'>America</button>
-          <button className='select-btn'>Asia</button>
-          <button className='select-btn'>Europe</button>
-          <button className='select-btn'>Oceania</button>
+          <button
+            className={`select-btn ${
+              !isLight ? 'select-btn-dark' : 'select - btn'
+            }`}
+          >
+            Africa
+          </button>
+          <button
+            className={`select-btn ${
+              !isLight ? 'select-btn-dark' : 'select - btn'
+            }`}
+          >
+            America
+          </button>
+          <button
+            className={`select-btn ${
+              !isLight ? 'select-btn-dark' : 'select - btn'
+            }`}
+          >
+            Asia
+          </button>
+          <button
+            className={`select-btn ${
+              !isLight ? 'select-btn-dark' : 'select - btn'
+            }`}
+          >
+            Europe
+          </button>
+          <button
+            className={`select-btn ${
+              !isLight ? 'select-btn-dark' : 'select - btn'
+            }`}
+          >
+            Oceania
+          </button>
         </div>
       </div>
     </div>
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ApiDataContext } from '../../dataProvider/DataProvider';
+
+import './homeStyle.css';
 
 import Nav from '../../components/nav/Nav';
 import Header from '../../components/header/Header';
 
 const HomePage = () => {
+  const { isLight } = useContext(ApiDataContext);
+
   return (
-    <div>
+    <div className={!isLight ? 'home-cnt-dark' : 'home-cnt'}>
       <Nav />
       <Header />
     </div>
