35f8b57a49223ebb8c66f682e9dabbd697c5f156
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-08 18:25:58 +0530

    worked on details page

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -10,8 +10,7 @@ const Details = () => {
 
   const { isLight, countryNameClicked } = useContext(ApiDataContext);
 
-  const [dtlData, setDtlData] = useState();
-  console.log('hi');
+  const [dtlData, setDtlData] = useState([]);
 
   useEffect(() => {
     try {
@@ -29,7 +28,6 @@ const Details = () => {
   const backBtnFunction = () => {
     navigate('/home');
   };
-
   return (
     <>
       <div>
@@ -40,18 +38,61 @@ const Details = () => {
         >
           Back
         </button>
-      </div>{' '}
+      </div>
       {dtlData && dtlData.length ? (
         <div>
           <div>
-            <div></div>
-            <div></div>
+            <div>
+              {dtlData[0].flags ? (
+                <img src={dtlData[0].flags.svg} alt={dtlData[0].flags.alt} />
+              ) : null}
+            </div>
+            <div>
+              <div>
+                <div>
+                  {dtlData[0].name ? <p>{dtlData[0].name.common}</p> : null}
+                </div>
+                <div>
+                  {dtlData[0].name.official ? (
+                    <p>
+                      <strong>Official Name:</strong> {dtlData[0].name.official}
+                    </p>
+                  ) : null}
+                  {dtlData[0].region ? (
+                    <p>
+                      <strong>Region:</strong> {dtlData[0].region}
+                    </p>
+                  ) : null}
+                  {dtlData[0].subregion ? (
+                    <p>
+                      <strong>Sub-Region:</strong> {dtlData[0].subregion}
+                    </p>
+                  ) : null}
+                  {dtlData[0].capital ? (
+                    <p>
+                      <strong>Capital:</strong> {dtlData[0].capital[0]}
+                    </p>
+                  ) : null}
+                </div>
+              </div>
+              <div>
+                {dtlData[0].tld ? (
+                  <p>
+                    <strong>Top Level Domain:</strong> {dtlData[0].tld[0]}
+                  </p>
+                ) : null}
+                {dtlData[0].currencies ? (
+                  <p>
+                    <strong>Population:</strong>
+                    {dtlData[0].population}
+                  </p>
+                ) : null}
+              </div>
+            </div>
           </div>
         </div>
       ) : (
-        <div>
-          <p>No details Found</p>
-        </div>
+        <div></div>
       )}
     </>
   );
