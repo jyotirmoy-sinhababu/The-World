7978dd3d2a1be53b3fb6cc6cd15225d4f3865efc
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-08 11:21:59 +0530

    style added to card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,19 +8,24 @@ const Card = ({ data }) => {
   const { isLight } = useContext(ApiDataContext);
 
   return (
-    <div>
-      <div>
+    <div className={!isLight ? 'main-card-cnt-dark' : 'main-card-cnt'}>
+      <div className='card-img-cnt'>
         <img src={data.flags.svg} className='card-img' alt={data.flags.alt} />
       </div>
-      <div>
-        <button onClick={(e) => {}}>{data.name.common}</button>
-        <p>
+      <div className='card-data-cnt'>
+        <button
+          className={!isLight ? 'card-btn-dark' : 'card-btn'}
+          onClick={(e) => {}}
+        >
+          {data.name.common}
+        </button>
+        <p className={!isLight ? 'card-data-dark' : 'card-data'}>
           <strong>Population</strong>: {data.population}
         </p>
-        <p>
+        <p className={!isLight ? 'card-data-dark' : 'card-data'}>
           <strong>Region:</strong> {data.region}
         </p>
-        <p>
+        <p className={!isLight ? 'card-data-dark' : 'card-data'}>
           <strong>Capital:</strong> {data.capital}
         </p>
       </div>
