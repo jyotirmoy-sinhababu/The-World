2a818f369bc1df8c7786c78650226f2c0f294501
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-10 12:41:52 +0530

    style added

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { ApiDataContext } from '../../dataProvider/DataProvider';
 
 import BtnCnt from '../../components/btnCnt/BtnCnt';
+import Error from '../error/Error';
 
 import './details.css';
 
@@ -29,97 +30,93 @@ const Details = () => {
     <div>
       <BtnCnt />
       {dtlData && dtlData.length ? (
-        <div>
-          <div className='main-details-cnt'>
-            <div className='details-img-cnt'>
-              {dtlData[0].flags ? (
-                <img
-                  className={`details-img ${
-                    !isLight ? 'details-img-dark' : null
-                  }`}
-                  src={dtlData[0].flags.svg}
-                  alt={dtlData[0].flags.alt}
-                />
-              ) : null}
+        <div className='main-details-cnt'>
+          <div className='details-img-cnt'>
+            {dtlData[0].flags ? (
+              <img
+                className={`details-img ${
+                  !isLight ? 'details-img-dark' : null
+                }`}
+                src={dtlData[0].flags.svg}
+                alt={dtlData[0].flags.alt}
+              />
+            ) : null}
+          </div>
+          <div className='details-data-cnt'>
+            <div className='details-data-firstcnt'>
+              <div>
+                {dtlData[0].name ? (
+                  <p
+                    className={`dtl-para-name ${
+                      !isLight ? 'dtl-para-dark' : 'dtl-para-name'
+                    }`}
+                  >
+                    {dtlData[0].name.common}
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                {dtlData[0].name.official ? (
+                  <p
+                    className={`dtl-para ${
+                      !isLight ? 'dtl-para-dark' : 'null'
+                    }`}
+                  >
+                    <strong>Official Name:</strong> {dtlData[0].name.official}
+                  </p>
+                ) : null}
+                {dtlData[0].region ? (
+                  <p
+                    className={`dtl-para ${
+                      !isLight ? 'dtl-para-dark' : 'null'
+                    }`}
+                  >
+                    <strong>Region:</strong> {dtlData[0].region}
+                  </p>
+                ) : null}
+                {dtlData[0].subregion ? (
+                  <p
+                    className={`dtl-para ${
+                      !isLight ? 'dtl-para-dark' : 'null'
+                    }`}
+                  >
+                    <strong>Sub-Region:</strong> {dtlData[0].subregion}
+                  </p>
+                ) : null}
+                {dtlData[0].capital ? (
+                  <p
+                    className={`dtl-para ${
+                      !isLight ? 'dtl-para-dark' : 'null'
+                    }`}
+                  >
+                    <strong>Capital:</strong> {dtlData[0].capital[0]}
+                  </p>
+                ) : null}
+              </div>
             </div>
-            <div className='details-data-cnt'>
-              <div className='details-data-firstcnt'>
-                <div>
-                  {dtlData[0].name ? (
-                    <p
-                      className={`dtl-para-name ${
-                        !isLight ? 'dtl-para-dark' : 'dtl-para-name'
-                      }`}
-                    >
-                      {dtlData[0].name.common}
-                    </p>
-                  ) : null}
-                </div>
-                <div>
-                  {dtlData[0].name.official ? (
-                    <p
-                      className={`dtl-para ${
-                        !isLight ? 'dtl-para-dark' : 'null'
-                      }`}
-                    >
-                      <strong>Official Name:</strong> {dtlData[0].name.official}
-                    </p>
-                  ) : null}
-                  {dtlData[0].region ? (
-                    <p
-                      className={`dtl-para ${
-                        !isLight ? 'dtl-para-dark' : 'null'
-                      }`}
-                    >
-                      <strong>Region:</strong> {dtlData[0].region}
-                    </p>
-                  ) : null}
-                  {dtlData[0].subregion ? (
-                    <p
-                      className={`dtl-para ${
-                        !isLight ? 'dtl-para-dark' : 'null'
-                      }`}
-                    >
-                      <strong>Sub-Region:</strong> {dtlData[0].subregion}
-                    </p>
-                  ) : null}
-                  {dtlData[0].capital ? (
-                    <p
-                      className={`dtl-para ${
-                        !isLight ? 'dtl-para-dark' : 'null'
-                      }`}
-                    >
-                      <strong>Capital:</strong> {dtlData[0].capital[0]}
-                    </p>
-                  ) : null}
-                </div>
-              </div>
-              <div className='details-data-secondcnt'>
-                {dtlData[0].tld ? (
-                  <p
-                    className={`dtl-para ${
-                      !isLight ? 'dtl-para-dark' : 'null'
-                    }`}
-                  >
-                    <strong>Top Level Domain:</strong> {dtlData[0].tld[0]}
-                  </p>
-                ) : null}
-                {dtlData[0].currencies ? (
-                  <p
-                    className={`dtl-para ${
-                      !isLight ? 'dtl-para-dark' : 'null'
-                    }`}
-                  >
-                    <strong>Population:</strong>
-                    {dtlData[0].population}
-                  </p>
-                ) : null}
-              </div>
+            <div className='details-data-secondcnt'>
+              {dtlData[0].tld ? (
+                <p
+                  className={`dtl-para ${!isLight ? 'dtl-para-dark' : 'null'}`}
+                >
+                  <strong>Top Level Domain:</strong> {dtlData[0].tld[0]}
+                </p>
+              ) : null}
+              {dtlData[0].currencies ? (
+                <p
+                  className={`dtl-para ${!isLight ? 'dtl-para-dark' : 'null'}`}
+                >
+                  <strong>Population:</strong>
+                  {dtlData[0].population}
+                </p>
+              ) : null}
             </div>
           </div>
         </div>
       ) : (
-        <div></div>
+        <div>
+          <Error />
+        </div>
       )}
     </div>
   );
--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ApiDataContext } from '../../dataProvider/DataProvider';
 
 import './error.css';
 
 const Error = () => {
+  const { isLight } = useContext(ApiDataContext);
+
   return (
-    <div>
-      <p>something went wrong...</p>
-      <p>Data Not Found</p>
+    <div
+      className={`error-cnt ${!isLight ? 'error-cnt-dark' : 'error-cnt-light'}`}
+    >
+      <p
+        className={`error-txt ${
+          !isLight ? 'error-txt-dark' : 'error-txt-light'
+        }`}
+      >
+        something went wrong...
+      </p>
+      <p
+        className={`error-txt ${
+          !isLight ? 'error-txt-dark' : 'error-txt-light'
+        }`}
+      >
+        Data Not Found
+      </p>
     </div>
   );
 };
